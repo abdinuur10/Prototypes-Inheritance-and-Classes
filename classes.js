2105c1dc0c1ef005355607aaed934515faadcda3
ABDIRASHID NUUR <78458279+abdinuur10@users.noreply.github.com>
2021-02-19 21:51:35 -0800

    almost done

--- classes.js
+++ classes.js
@@ -6,5 +6,71 @@
 // Give it "Name", "Age", "Hobby" properties
 // Also give it a method "Speak", which says "Hello, My name is + name + and I'm + age + years old. I love + Hobby"
 
+function Person(attributes) {
+    this.age = attributes.age;
+    this.myName = attributes.myName;
+    this.Hobby = attributes.Hobby;
+    this.speak = function () {
+      return `Hello, my name is ${this.myName} and I am ${this.age}, I love playing ${this.Hobby}`;
+    };
+  }
+  
+  const Mohamed = new Person({
+    age: 55,
+    myName: "Mohamed",
+    Hobby: "Soccer"
+  });
+  
+  function child(childAttributes) {
+    Person.call(this, childAttributes);
+    console.log(childAttributes);
+  }
+  
+  child.prototype = Object.create(Person.prototype);
+  
+  const Abdirahman = new child({
+    age: 12,
+    myName: "Abdirahman",
+    Hobby: "Baseball"
+  });
+  
+  const Siham = new child({
+    age: 18,
+    myName: 'Siham',
+    Hobby: 'Batman'
+  });
+  
+  const Muna = new child({
+    age: 17,
+    myName: 'Muna',
+    Hobby: 'Quiter'
+  });
+  
+  console.log(Mohamed.speak());
+  console.log(Abdirahman.speak());
+  console.log(Siham.speak());
+  console.log(Muna.speak());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Extend Person class and create one class for each member of your family.
--- this.js
+++ this.js
@@ -1,10 +1,13 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding: it's global scoop the value and located in the window console object
+* 2. Implicit Binding: whenevr preceding dot calls a funtion, then the object before the dot, and function is invoked.
+* 3. New Binding: when a function is use new keyword to construct a new object, and also when a function is invoked as a constructor function using new keyword 'this' point to the new object that was created.
+* 4. explicit Binding: explict binding has 3 different faces, such as call,apply and Bind.
+                       - call will immediately invoke the function and pass through the argument one by one
+                       - apply will immediatly invoke function and pass array as an argument
+                       - Bind- it will pass in the argument 1 by 1 but it does not invoke the function immediatly instead it return a new function that you can able to invoke later in time.
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +15,52 @@
 // Principle 1
 
 // code example for Window Binding
+function saySport(sport) {
+    console.log(this);
+    return sport
+}
+saySport("soccer")
 
 // Principle 2
 
 // code example for Implicit Binding
+const teacher = {
+    name: 'Micheal',
+    city: "Ohio",
+    sayHello: function() {
+        console.log(`Hello my name is ${this.name}`)
+    }
+
+}
+
+teacher.sayHello()
+
+
 
 // Principle 3
 
 // code example for New Binding
+function Coach(mySport, city) {
+    this.mySport = mySport;
+    this.city = city;
+}
+const CoachOne = New Coach('Alex', 'Seattle')
+console.log(CoachOne.mySport)
 
 // Principle 4
 
 // code example for Explicit Binding
+// .call function
+function Emlpoyee() {
+    console.log(this.Emlpoyee)
+    
+}
+const EmoloyeeOne = {
+    myName: 'Tarafa'
+}
+Emlpoyee.call(EmoloyeeOne)
+
+// .bind function
+
+const FirstEmoloyee = Emoloyee.bind(EmoloyeeOne)
+//  FirstEmloyee is the new Variable bind into the function.
